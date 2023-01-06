a5eb6a65d55dc5dc79348c141298be4f2f9e816f
Samuel Son <Samuel.Daehan@gmail.com>
2023-01-06 00:07:20 -0600

    register page styling updated

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -2,7 +2,10 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { signUp } from "../redux/reducers/AuthSlice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "./registerpage.css";
 
 const RegisterPage = () => {
 	const [firstName, setFirstName] = useState("");
@@ -36,32 +39,64 @@ const RegisterPage = () => {
 	}, [navigate, userInfo, success]);
 
 	return (
-		<div>
-			<input
-				type="text"
-				placeholder="first name"
-				onChange={(event) => setFirstName(event.target.value)}
-			/>
-			<input
-				type="text"
-				placeholder="last name"
-				onChange={(event) => setLastName(event.target.value)}
-			/>
-			<input
-				type="email"
-				placeholder="email"
-				onChange={(event) => setEmail(event.target.value)}
-			/>
-			<input
-				type="password"
-				placeholder="password"
-				onChange={(event) => setPassword(event.target.value)}
-			/>
-			<button onClick={createUser} disabled={loading}>
-				create a user
-			</button>
-			{error && <div>{error}</div>}
-		</div>
+		<>
+			<div className="register-container">
+				<div className="register-wrapper">
+					<div className="register-inner-wrapper">
+						<Form>
+							<Form.Group className="mb-3" controlId="formBasicEmail">
+								<Form.Label>Email address</Form.Label>
+								<Form.Control
+									type="text"
+									placeholder="Enter first name"
+									onChange={(event) => setFirstName(event.target.value)}
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="formBasicPassword">
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									type="text"
+									placeholder="Enter last name"
+									onChange={(event) => setLastName(event.target.value)}
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="formBasicEmail">
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									type="email"
+									placeholder="Enter email"
+									onChange={(event) => setEmail(event.target.value)}
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="formBasicPassword">
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									type="password"
+									placeholder="Enter password"
+									onChange={(event) => setPassword(event.target.value)}
+								/>
+							</Form.Group>
+
+							<Button
+								type="submit"
+								onClick={createUser}
+								className="register-button"
+							>
+								Submit
+							</Button>
+						</Form>
+						{error && <div>{error}</div>}
+						<hr />
+						<div className="register-inner-wrapper-bottom">
+							<h4>Already have an account?</h4>
+							<Link to="/login">
+								<h4>Login</h4>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
